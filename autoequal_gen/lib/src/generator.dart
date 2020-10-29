b71e07bab24021f0e13b5d328264d7a1fa1c4b71
Aleksandr Tcikin <mail@sunnyday.dev>
2020-10-29 18:01:02 +0300

    Generator fixed.

--- autoequal_gen/lib/src/generator.dart
+++ autoequal_gen/lib/src/generator.dart
@@ -29,14 +29,11 @@ class AutoequalGenerator extends GeneratorForAnnotation<Autoequal> {
   }
 
   ClassElement _ensureReadyForAutoequalClass(ClassElement element) {
-    if (element is! ClassElement)
-      throw '$element is not a ClassElement';
+    if (element is! ClassElement) throw "$element is not a ClassElement";
 
-    if (element.isAbstract)
-      throw '$element is abstract. Autoequal doesn\'t support abstract classes.';
+    if (element.isAbstract) throw "$element is abstract. Autoequal doesn't support abstract classes.";
 
-    if (_isNotUseEquatable(element))
-      throw '$element is not Equatable or EquatableMixin';
+    if (_isNotUseEquatable(element)) throw "$element is not Equatable or EquatableMixin";
 
     return element;
   }
@@ -46,9 +43,7 @@ class AutoequalGenerator extends GeneratorForAnnotation<Autoequal> {
 
     if (isGenerateMixin) {
       return _AutoequalMixinTemplate.print(
-        name: element.name,
-        onType: _isEquatable(element) ? "Equatable" : "EquatableMixin"
-      );
+          name: element.name, onType: _isEquatable(element) ? "Equatable" : "EquatableMixin");
     } else {
       return null;
     }
@@ -57,55 +52,37 @@ class AutoequalGenerator extends GeneratorForAnnotation<Autoequal> {
   String _generateExtension(ClassElement element) {
     final className = element.name;
 
-    final autoEqualFields = element.fields
-        .where((field) => _isNotIgnoredField(field))
-        .map((e) => e.name);
+    final autoEqualFields = element.fields.where((field) => _isNotIgnoredField(field)).map((e) => e.name);
 
-    return _AutoequalExtensionTemplate.print(
-        name: className,
-        props: autoEqualFields
-    );
+    return _AutoequalExtensionTemplate.print(name: className, props: autoEqualFields);
   }
 
-  bool _isNotIgnoredField(FieldElement element) =>
-      !(element.isStatic
-          || element.name == "props"
-          || _ignore.hasAnnotationOfExact(element)
-          || _ignore.hasAnnotationOfExact(element.getter)
-      );
+  bool _isNotIgnoredField(FieldElement element) => !(element.isStatic ||
+      element.name == "props" ||
+      _ignore.hasAnnotationOfExact(element) ||
+      _ignore.hasAnnotationOfExact(element.getter));
 
-  bool _isNotUseEquatable(ClassElement element) =>
-      !(_isEquatable(element) || _isWithEquatableMixin(element));
+  bool _isNotUseEquatable(ClassElement element) => !(_isEquatable(element) || _isWithEquatableMixin(element));
 
-  bool _isEquatable(ClassElement element) =>
-      _equatable.isSuperOf(element);
+  bool _isEquatable(ClassElement element) => _equatable.isSuperOf(element);
 
   bool _isWithEquatableMixin(ClassElement element) =>
       element.mixins.any((type) => _equatableMixin.isExactly(type.element));
 }
 
 class _AutoequalMixinTemplate {
-  static String print({
-    String name,
-    String onType
-  }) =>
-      """
+  static String print({String name, String onType}) => """
       mixin _\$${name}AutoequalMixin on $onType {
         @override
         List<Object> get props =>
-          _\$${name}Autoequal(this)._autoequalProps;
+          _\$${name}Autoequal(this as $name)._autoequalProps;
       }
       """;
 }
 
 class _AutoequalExtensionTemplate {
-  static String print({
-    String name,
-    Iterable<String> props
-  }) =>
-      """
+  static String print({String name, Iterable<String> props}) => """
       extension _\$${name}Autoequal on $name {
-        // ignore: unused_element
         List<Object> get _autoequalProps =>
             [${props.join(", ")}];
       }
