62f28ae12018fc4e9b5748b9dbeb3fec343f8e53
Morgan <mrgnhnt96@gmail.com>
2022-10-26 12:21:16 -0500

    feat: create settings from build.yaml file adds support to include getters, is false by default

--- autoequal_gen/lib/autoequal_gen.dart
+++ autoequal_gen/lib/autoequal_gen.dart
@@ -1,9 +1,20 @@
 library autoequal_gen;
 
+import 'package:autoequal_gen/src/settings.dart';
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 
 import 'src/generator.dart';
 
-Builder autoequalGenerator(BuilderOptions options) =>
-    SharedPartBuilder([AutoequalGenerator()], 'autoequal_gen');
+late Settings settings;
+
+Builder autoequalGenerator(BuilderOptions options) {
+  settings = Settings.fromConfig(options.config);
+
+  return SharedPartBuilder(
+    [
+      AutoequalGenerator(),
+    ],
+    'autoequal_gen',
+  );
+}
