4469e97aba34baf6641f3cb55d0cea9d723e56eb
Morgan <mrgnhnt96@gmail.com>
2022-10-26 13:48:37 -0500

    feat: add example for extends and mixin

--- autoequal_gen/example/main.dart
+++ autoequal_gen/example/main.dart
@@ -3,17 +3,35 @@ import 'package:equatable/equatable.dart';
 
 part 'main.g.dart';
 
-@autoequalMixin
-class ExampleClass extends Equatable with _$ExampleClassAutoequalMixin {
+@autoequal
+class Example1Class extends Equatable {
+  Example1Class({
+    required this.value,
+    this.ignored,
+    this.optional,
+  });
+
   final String value;
   final String? optional;
 
   @ignoreAutoequal
   final String? ignored;
 
-  ExampleClass({
+  @override
+  List<Object?> get props => _$props;
+}
+
+@autoequal
+class Example2Class with EquatableMixin, _$Example2ClassAutoequalMixin {
+  const Example2Class({
     required this.value,
     this.ignored,
     this.optional,
   });
+
+  final String value;
+  final String? optional;
+
+  @ignoreAutoequal
+  final String? ignored;
 }
